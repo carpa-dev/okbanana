9291378cafc29b98e5644bb55a2bdcd6923ad61c
Eduardo Aleixo <eduardo@carpa.dev>
2024-11-15 19:38:34 +0000

    compra success

--- src/compra-2/script.ts
+++ src/compra-2/script.ts
@@ -1,2 +1,57 @@
 import './style.css';
-console.log('hey');
+
+function sendItHome() {
+  location.href = '/';
+}
+
+document.addEventListener(
+  'DOMContentLoaded',
+  function () {
+    const urlParams = new URLSearchParams(window.location.search);
+    const item = urlParams.get('item');
+
+    if (!item) {
+      sendItHome();
+      return;
+    }
+
+    const query = document.querySelector('#query');
+    const itemInput =
+      document.querySelector<HTMLInputElement>('#item-input-hidden');
+
+    if (!query || !itemInput) {
+      alert('Algo de errado aconteceu!');
+      sendItHome();
+      return;
+    }
+
+    query.textContent = item;
+    itemInput.value = item;
+  },
+  false
+);
+
+document
+  .querySelector<HTMLFormElement>('#form')
+  ?.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if (!e.currentTarget) {
+      console.warn("Could not find 'currentTarget' of submitted form");
+      return;
+    }
+
+    const form = new FormData(e.currentTarget as HTMLFormElement);
+    const email = form.get('email')?.toString();
+    if (!email) {
+      console.warn("Could not find field 'item' of submitted form");
+      return;
+    }
+
+    const searchParams = new URLSearchParams('');
+    searchParams.set('email', email);
+
+    // TODO: submit this to an API
+    // Then redirect to success
+    location.href = '/compra-success/?' + searchParams.toString();
+  });
