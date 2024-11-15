2eee7eb4134cbb6d72345da96df58e1c91e94da3
Eduardo Aleixo <eduardo@carpa.dev>
2024-11-15 18:32:40 +0000

    mpa?

---
+++ README.md
@@ -0,0 +1,12 @@
+# Ok Banana
+
+```bash
+npm install
+npm run dev
+```
+
+
+## FAQ
+### Adding New Pages
+It must be added to `src`, this is due to the lack of multi-page support in Vite.
+See https://github.com/vitejs/vite/issues/3429#issuecomment-1964555854 for more info.
--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,18 @@
 import { resolve } from 'path';
 import handlebars from 'vite-plugin-handlebars';
-import { fdir } from 'fdir';
 import { defineConfig } from 'vite';
+import glob from 'fast-glob';
 
-// Couldn't use glob for some reason
-//const pages = new fdir()
-//  .filter((path, isDirectory) => path.endsWith('.html'))
-//  .crawl('.')
-//  .sync()
-//  .reduce((acc, curr) => {
-//    acc[curr] = resolve(__dirname, curr);
-//    return acc;
-//  }, {});
-//
-//console.log(mpa.default);
 export default defineConfig({
+  root: 'src',
   build: {
+    outDir: '../dist',
+
     rollupOptions: {
-      // https://github.com/vitejs/vite/issues/3429
-      // multi page support is crap
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        'sobre-nos': resolve(__dirname, 'sobre-nos.html'),
-      },
+      // https://github.com/vitejs/vite/issues/3429#issuecomment-1964555854
+      input: glob
+        .sync(['src/**/*.html', '!src/partials'])
+        .map((entry) => resolve(__dirname, entry)),
     },
   },
   plugins: [
