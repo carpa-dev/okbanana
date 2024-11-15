6941938357123e1312e08ddb2c925799cb410c6c
Eduardo Aleixo <eduardo@carpa.dev>
2024-11-15 15:33:15 +0000

    allow MPA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
 import { resolve } from 'path';
 import handlebars from 'vite-plugin-handlebars';
+import { fdir } from 'fdir';
+
+// Couldn't use glob for some reason
+const pages = new fdir()
+  .filter((path, isDirectory) => path.endsWith('.html'))
+  .crawl('pages')
+  .sync()
+  .reduce((acc, curr) => {
+    acc[curr] = resolve(__dirname, 'pages', curr);
+    return acc;
+  }, {});
 
 export default {
+  build: {
+    rollupOptions: {
+      input: {
+        ...pages,
+      },
+    },
+  },
   plugins: [
     handlebars({
       partialDirectory: resolve(__dirname, 'src/partials'),
