85b1d8b2317504f44191e87acf5f97d27173642f
Eduardo Aleixo <eduardo@carpa.dev>
2024-11-16 18:55:58 +0000

    fix top level await

--- src/vender/script.ts
+++ src/vender/script.ts
@@ -1,44 +1,48 @@
 import './style.css';
 import { loadCompras } from '../services/db';
 
-const data = await loadCompras();
+async function run() {
+  const data = await loadCompras();
 
-document.querySelector('#loader')?.classList.add('_hidden');
+  document.querySelector('#loader')?.classList.add('_hidden');
 
-const table = document.querySelector('table');
+  const table = document.querySelector('table');
 
-const tbody = document.createElement('tbody');
-const rows = data.map((d) => {
-  const row = document.createElement('tr');
+  const tbody = document.createElement('tbody');
+  const rows = data.map((d) => {
+    const row = document.createElement('tr');
 
-  const itemTd = document.createElement('td');
-  itemTd.innerText = d.item;
+    const itemTd = document.createElement('td');
+    itemTd.innerText = d.item;
 
-  const cityState = document.createElement('td');
-  cityState.innerText = d.cityState;
+    const cityState = document.createElement('td');
+    cityState.innerText = d.cityState;
 
-  const minValue = document.createElement('td');
-  minValue.innerText = d.minValue;
+    const minValue = document.createElement('td');
+    minValue.innerText = d.minValue;
 
-  const maxValue = document.createElement('td');
-  maxValue.innerText = d.maxValue;
+    const maxValue = document.createElement('td');
+    maxValue.innerText = d.maxValue;
 
-  const desc = document.createElement('td');
-  desc.innerText = d.desc;
+    const desc = document.createElement('td');
+    desc.innerText = d.desc;
 
-  row.appendChild(itemTd);
-  row.appendChild(cityState);
-  row.appendChild(minValue);
-  row.appendChild(maxValue);
-  row.appendChild(desc);
+    row.appendChild(itemTd);
+    row.appendChild(cityState);
+    row.appendChild(minValue);
+    row.appendChild(maxValue);
+    row.appendChild(desc);
 
-  return row;
-});
+    return row;
+  });
 
-rows.forEach((r) => {
-  tbody.appendChild(r);
-});
+  rows.forEach((r) => {
+    tbody.appendChild(r);
+  });
 
-table?.appendChild(tbody);
+  table?.appendChild(tbody);
 
-document.querySelector('#content')?.classList.remove('_hidden');
+  document.querySelector('#content')?.classList.remove('_hidden');
+}
+
+run();
